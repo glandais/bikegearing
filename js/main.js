270f860c721ec88145b2b997e1277202867c47a1
Gabriel Landais <gabriel.landais@gmail.com>
2023-10-29 16:11:09 +0100

    Rivets

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 
+var debug = true;
+
 var state = {
     f: 50, // teeth front
     r: 15, // teeth rear
     cs: 410, // chainstay (mm)
-    cl: 50, // chain length (mm -> cl * 2.54)
+    cl: 100, // chain length in rivets (mm -> cl * 2.54 / 2)
     af: 0, // angle front
     ar: 0, // angle rear
-    fu: 18, // cog hole number where chain is leaving front
+    fu: 14, // cog hole number where chain is leaving front
     cfu: 18, // rivet number on fu
     ru: 4, // cog hole number where chain is arriving on rear
     cru: 48, // rivet number on ru
-    clf: 36, // rivets on front
-    clr: 8 // rivets on rear
+    clf: 27, // rivets on front
+    clr: 6 // rivets on rear
 };
+
+let cfu = state.cfu;
+let cru = state.cru;
 
 function init() {
     canvas = document.getElementById("canvas");
@@ -22,7 +27,15 @@ function init() {
     initInteractive();
     draw();
     setInterval(() => {
-        // state.af = state.af + 0.001;
-        // state.ar = state.ar + 0.001 * (state.f / state.r);
+        state.af = state.af + 0.001;
+        cfu = cfu + state.f * 0.001 / (2 * Math.PI);
+        state.cfu = Math.round(cfu);
+        state.fu = Math.round(14 + state.f * state.af / (2 * Math.PI));
+
+        const dar = 0.001 * (state.f / state.r);
+        state.ar = state.ar + dar;
+        cru = cru + state.r * dar / (2 * Math.PI);
+        state.cru = Math.round(cru);
+        state.ru = Math.round(4 + state.r * state.ar / (2 * Math.PI));
     }, 10);
 }
